937ac8b775bc40f6db7c4acd04193fcebbef289e
Rodrigo Herrán <rodrihc@users.noreply.github.com>
2020-03-26 23:10:46 +0100

    inital commit

--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -8,24 +8,24 @@ public class Constants {
 
     public static final List<String> HASHTAGS = Lists.newArrayList("#covid", "#covid_19", "#coronavirus", "#CoronavirusPandemic", "#covid_19DE", "#COVID19outbreak");
     //cosmos db
-    public static final String COSMO_HOST = "https://we-komentity-cosmos.documents.azure.com:443/";
-    public static final String COSMO_MASTER_KEY = "8VcrOeTDyp5tDmkb2ty4q7POBEEBzs2uMJFLAD2taF599ChIwQ4acCz4N6ChnyCT7rgNaLHHH3Hb4LO7B9oFzA==";
+    public static final String COSMO_HOST = "";
+    public static final String COSMO_MASTER_KEY = "";
 
     //Elasticsearch Access Keys (Bonsai)
-    public final static String PASSWORD = "rmbmein93z"; // needed only for bonsai
+    public final static String PASSWORD = ""; // needed only for bonsai
 
     //Twitter Access Keys
     // Consumer API keys
 
     //(API key)
-    public static final String CONSUMER_KEY = "KF7Dci9CEpBJrhMWJ0a7j8wOj";
+    public static final String CONSUMER_KEY = "";
     //(API secret key)
-    public static final String CONSUMER_SECRET = "70CrKBeKYNhYDrYqFdu7jsfa82asN08TMHOawnd5xsg4ieCzLG";
+    public static final String CONSUMER_SECRET = "";
 
     //Access token & access token secret
 
     //(Access token)
-    public static final String TOKEN = "61317543-NeuIBWE2QjvJRiob0jaVGHamhX27pnMkMdmqjaYj8";
+    public static final String TOKEN = "";
     // (Access token secret)
-    public static final String SECRET = "5bZxxispTGvxtKdH6iBLhx1XkhulYEFEAAGefRSkDmQwI";
+    public static final String SECRET = "";
 }
